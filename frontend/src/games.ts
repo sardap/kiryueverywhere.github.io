e44305a26840f1ea52b75cde752f4c151c60aa72
sardap <sardap@users.noreply.github.com>
2022-08-13 19:33:44 +1000

    added up until day 24

--- frontend/src/games.ts
+++ frontend/src/games.ts
@@ -80,5 +80,29 @@ export const games: Record<number, gameInfo> = {
   18: {
     map: "yakuza_kiwami_map",
     target: { x: 40.537318914097945, y: 39.22800718132854 },
+  },
+  19: {
+    map: "yakuza_0_osaka_map",
+    target: { x: 6.21967840572853, y: -5.716878402903811 },
+  },
+  20: {
+    map: "yakuza_4_kamuro_streets_map",
+    target: { x: -36.519217408073715, y: 8.076225045372052 },
+  },
+  21: {
+    map: "yakuza_dead_souls_kamuro_streets_map",
+    target: { x: 7.465254387285787, y: 46.91470054446461 },
+  },
+  22: {
+    map: "yakuza_kiwami_map",
+    target: { x: 34.73436194846733, y: -19.509981851179674 },
+  },
+  23: {
+    map: "yakuza_kiwami_2_osaka_map",
+    target: { x: 27.04893308863395, y: -6.624319419237749 },
+  },
+  24: {
+    map: "yakuza_0_kamuro_map",
+    target: { x: 11.149825783972126, y: 3.720508166969147 },
   }
 };
